2b3bb94fe195707c508de54569d4d748e12e6c64
Pepe García <pepe@pepegar.com>
2018-07-11 18:16:31 +0200

    add exercise description

---
+++ exercises/exercise1/src/main/scala/eventsourced.scala
@@ -0,0 +1,14 @@
+package exercise1
+
+/**
+  * Let's imagine a simple event sourced application.  We want to define
+  * some events that we can handle:
+  *
+  * - An user logs in
+  * - A customer adds an item to the basket
+  * - An user starts payment process
+  * - Payment goes through correctly
+  * - Payment process fails with timeout
+  * - Payment process fails because of Insufficent funds
+  */
+sealed trait Event
