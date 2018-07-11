a6182deb27d761be2ee3a75aa5a005ddb3fb1a07
Pepe García <pepe@pepegar.com>
2018-07-11 18:35:03 +0200

    add description for height

--- exercises/exercise2/src/main/binarytree.scala
+++ exercises/exercise2/src/main/binarytree.scala
@@ -6,7 +6,18 @@ package exercise2
   *
   * Binary trees are data structures in which each node has a value,
   * an two children at most.
+  * 
+  * https://en.wikipedia.org/wiki/Binary_tree
   */
 sealed trait Tree[A]
 case class Empty[A]() extends Tree[A]
 case class Node[A](l: Tree[A], a: A, r: Tree[A]) extends Tree[A]
+
+object Tree {
+
+  /**
+   * Implement a function height that returns the longest height in a
+   * tree
+   */
+  def height[A](tree: Tree[A]): Int = ???
+}
