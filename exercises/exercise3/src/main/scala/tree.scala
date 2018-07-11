870021af26e7678479ea35bf82529855026db607
Pepe García <pepe@pepegar.com>
2018-07-11 19:07:25 +0200

    exercise 3.2 description

--- exercises/exercise3/src/main/scala/tree.scala
+++ exercises/exercise3/src/main/scala/tree.scala
@@ -17,4 +17,39 @@ object Tree {
       fold(right)(onEmpty, onNode)
     )
   }
+
+  /**
+    * Refactor all the functions we implemented with primitive recursion 
+    */
+  def height[A](tree: Tree[A]): Int = tree match {
+    case Empty() => 0
+    case Node(l, _, r) => 1 + (height(l).max(height(r)))
+  }
+
+  def sum(tree: Tree[Int]): Int = tree match {
+    case Empty() => 0
+    case Node(l, x, r) => x + sum(l) + sum(r)
+  }
+
+  def count[A](tree: Tree[A]): Int =  tree match {
+    case Empty() => 0
+    case Node(l, _, r) => 1 + count(l) + count(r)
+  }
+
+  def toStringNodes(tree: Tree[Int]): Tree[String] = tree match {
+    case Empty() => Empty()
+    case Node(l, x, r) => Node(
+      toStringNodes(l),
+      x.toString,
+      toStringNodes(r))
+  }
+
+  def squared(tree: Tree[Int]): Tree[Int] = tree match {
+    case Empty() => Empty()
+    case Node(l, x, r) => Node(
+      squared(l),
+      x * x,
+      squared(r))
+  }
+
 }
