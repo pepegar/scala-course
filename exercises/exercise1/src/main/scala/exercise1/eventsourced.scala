877c831c1f853cfeb754e83c076c4e06db021dcd
Pepe García <pepe@pepegar.com>
2020-04-22 01:44:03 +0200

    exercise 1.1 solution

--- exercises/exercise1/src/main/scala/exercise1/eventsourced.scala
+++ exercises/exercise1/src/main/scala/exercise1/eventsourced.scala
@@ -14,3 +14,11 @@ import java.util.UUID
   * - Payment process fails because of Insufficent funds
   */
 sealed trait Event
+case class UserLogIn(userId: UUID) extends Event
+case class AddItemToBasket(userId: UUID, itemId: UUID) extends Event
+case class UserIntentPay(userId: UUID) extends Event
+case class PaymentCorrect(userId: UUID, paymentReceipt: String) extends Event
+
+sealed trait PaymentFailure extends Event
+case class TimeoutFailure(userId: UUID, intentId: UUID) extends PaymentFailure
+case class InsufficentFundsFailure(userId: UUID, inteintId: UUID) extends PaymentFailure
